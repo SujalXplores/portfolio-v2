9ef2bf3a295097546fa04200bf0f5a80dff280e8
Sujal Shah <sujal@zuru.com>
2023-12-02 23:42:26 +0530

    update: reset button

--- app/error.tsx
+++ app/error.tsx
@@ -17,6 +17,13 @@ export default function Error({
   return (
     <div>
       <p>Oh no, something went wrong... maybe refresh?</p>
+      <button
+        onClick={reset}
+        type="button"
+        className="flex items-center justify-center mt-3 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
+      >
+        Reset
+      </button>
     </div>
   );
 }
