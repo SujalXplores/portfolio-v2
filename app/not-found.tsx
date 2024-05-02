4044563a56cba6c10892dfa1eb38459318425709
Sujal Shah <sujal@zuru.com>
2024-05-02 16:02:14 +0530

    Add NotFound component to handle non-existent pages

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight text-white mb-4">
+        The page you are looking for does not exist.
+      </h1>
+      <p></p>
+      <p>Please check the URL and try again.</p>
+    </>
+  );
+}
